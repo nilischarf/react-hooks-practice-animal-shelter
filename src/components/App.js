2bd1ca33ee968e5261d831a2feedb8581aae0efc
Nili Scharf <nilischarf@gmail.com>
2025-01-28 15:15:46 +0200

    complete animal shelter

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,28 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function handleChangeType(type) {
+    setFilters({ type })
+  }
+
+  function handleFindPetsClick() {
+    let endpoint = "http://localhost:3001/pets"
+    if (filters.type !== "all") {
+      endpoint += `?type=${filters.type}`
+    }
+    fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => setPets(data))
+  }
+
+  function handleAdoptPet(id) {
+    setPets((prevPets) => 
+      prevPets.map((pet) => 
+        pet.id === id ? { ...pet, isAdopted: true } : pet 
+      )
+    )
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +37,16 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters 
+              onChangeType={handleChangeType}
+              onFindPetsClick={handleFindPetsClick}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser 
+              pets={pets}
+              onAdoptPet={handleAdoptPet}
+            />
           </div>
         </div>
       </div>
